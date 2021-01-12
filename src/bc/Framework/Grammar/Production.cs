bb549fc58246a1e9873f5072a4d23261823b372e
Brad Bodily <bradbodily@hotmail.com>
2021-01-11 23:34:47 -0500

    Added some tests/operators

--- src/bc/Framework/Grammar/Production.cs
+++ src/bc/Framework/Grammar/Production.cs
@@ -61,10 +61,7 @@ namespace bc.Framework.Grammar
         public int CompareTo(Production other)
         {
             var comp = Predecessor.CompareTo(other.Predecessor);
-            if (comp != 0)
-                return comp;
-
-            return Successor.CompareTo(other.Successor);
+            return comp != 0 ? comp : Successor.CompareTo(other.Successor);
         }
 
         /// <summary>
@@ -73,5 +70,55 @@ namespace bc.Framework.Grammar
         /// <param name="other">the <see cref="Production"/> value to compare with this value</param>
         /// <returns><c>true</c> if <paramref name="other"/> is equal to this value; otherwise, false</returns>
         public bool Equals(Production other) => Predecessor.Equals(other.Predecessor) && Successor.Equals(other.Successor);
+
+        /// <summary>
+        /// Compares two <see cref="Production"/> values for equality
+        /// </summary>
+        /// <param name="lhs">the first value</param>
+        /// <param name="rhs">the second value</param>
+        /// <returns><c>true</c> if <paramref name="lhs"/> and <paramref name="rhs"/> are equal values; otherwise, <c>false</c></returns>
+        public static bool operator ==(Production lhs, Production rhs) => lhs.Predecessor.Equals(rhs.Predecessor) && lhs.Successor.Equals(rhs.Successor);
+
+        /// <summary>
+        /// Compares two <see cref="Production"/> values for inequality
+        /// </summary>
+        /// <param name="lhs">the first value</param>
+        /// <param name="rhs">the second value</param>
+        /// <returns><c>true</c> if <paramref name="lhs"/> and <paramref name="rhs"/> are not equal values; otherwise, <c>false</c></returns>
+        public static bool operator !=(Production lhs, Production rhs) => !lhs.Predecessor.Equals(rhs.Predecessor) || !lhs.Successor.Equals(rhs.Successor);
+
+        public static bool operator <(Production left, Production right)
+        {
+            return left.CompareTo(right) < 0;
+        }
+
+        public static bool operator >(Production left, Production right)
+        {
+            return left.CompareTo(right) > 0;
+        }
+
+        public static bool operator <=(Production left, Production right)
+        {
+            return left.CompareTo(right) <= 0;
+        }
+
+        public static bool operator >=(Production left, Production right)
+        {
+            return left.CompareTo(right) >= 0;
+        }
+
+        /// <summary>
+        /// Determines whether this value and another <see cref="Object"/> are equal values
+        /// </summary>
+        /// <param name="obj">the other object</param>
+        /// <returns><c>true</c> if <paramref name="obj"/> is a <see cref="Production"/> value equal to this value; otherwise, <c>false</c></returns>
+        public override bool Equals(object obj) => obj is Production other && this.Equals(other);
+
+        /// <summary>
+        /// Calculates the hash code for a <see cref="Production"/> value
+        /// </summary>
+        /// <returns>the hash code for this value</returns>
+        public override int GetHashCode() => (Predecessor, Successor).GetHashCode();
     }
+
 }
--- test/bc.Tests/Framework/Grammar/ProductionTests.cs
+++ test/bc.Tests/Framework/Grammar/ProductionTests.cs
@@ -234,6 +234,300 @@ namespace bc.Framework.Grammar
         }
 
         /// <summary>
+        /// Verifies <see cref="Production.Equals(object)"/>, when the other object is null, properly returns <c>false</c>
+        /// </summary>
+        [TestMethod]
+        public void EqualsObject_other_is_null_returns_false()
+        {
+            var prod = new Production { Predecessor = "a", Successor = "b" };
+            Object other = null;
+
+            Assert.IsNull(other);
+
+            var expected = false;
+            var actual = prod.Equals(other);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        /// <summary>
+        /// Verifies <see cref="Production.Equals(object)"/>, when the other object is an equal <see cref="Production"/>, properly returns <c>true</c>
+        /// </summary>
+        [TestMethod]
+        public void EqualsObject_other_is_equal_Production_returns_true()
+        {
+            var predessor = "a";
+            var successor = "b";
+
+            var production = new Production { Predecessor = predessor, Successor = successor };
+            Object other = new Production { Predecessor = predessor, Successor = successor };
+
+            Assert.AreEqual(production, (Production)other);
+
+            var expected = true;
+            var actual = production.Equals(other);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        /// <summary>
+        /// Verifies <see cref="Production.Equals(object)"/>, when the other object is a <see cref="Production"/> with an different <see cref="Production.Predecessor"/>, properly returns <c>false</c>
+        /// </summary>
+        [TestMethod]
+        public void EqualsObject_other_is_Production_with_different_Predessor_returns_false()
+        {
+            var predessor1 = "a";
+            var predessor2 = "b";
+            var successor = "c";
+
+            var production = new Production { Predecessor = predessor1, Successor = successor };
+            Object other = new Production { Predecessor = predessor2, Successor = successor };
+
+            Assert.AreNotEqual(production.Predecessor, ((Production)other).Predecessor);
+
+            var expected = false;
+            var actual = production.Equals(other);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        /// <summary>
+        /// Verifies <see cref="Production.Equals(object)"/>, when the other object is a <see cref="Production"/> with an different <see cref="Production.Successor"/>, properly returns <c>false</c>
+        /// </summary>
+        [TestMethod]
+        public void EqualsObject_other_is_Production_with_different_Successor_returns_false()
+        {
+            var predessor = "a";
+            var successor1 = "b";
+            var successor2 = "c";
+
+            var production = new Production { Predecessor = predessor, Successor = successor1 };
+            Object other = new Production { Predecessor = predessor, Successor = successor2 };
+
+            Assert.AreNotEqual(production.Successor, ((Production)other).Successor);
+
+            var expected = false;
+            var actual = production.Equals(other);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        /// <summary>
+        /// Verifies <see cref="Production.Equals(object)"/>, when the other object is not a <see cref="Production"/>, properly returns <c>false</c>
+        /// </summary>
+        [TestMethod]
+        public void EqualsObject_other_isNot_Production_returns_false()
+        {
+            var prod = new Production { Predecessor = "a", Successor = "b" };
+            String other = "";
+
+            Assert.IsNotInstanceOfType(other, typeof(Production));
+
+            var expected = false;
+            var actual = prod.Equals(other);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        /// <summary>
+        /// Verifies <see cref="Production.GetHashCode"/> properly returns the same hash code for equal values
+        /// </summary>
+        /// <param name="predessor">a predessor value</param>
+        /// <param name="successor">a successor value</param>
+        [DataTestMethod]
+        [DataRow("a", "b")]
+        [DataRow("c", "d")]
+        public void GetHashCode_equal_values_return_equal_hashcodes(string predessor, string successor)
+        {
+            var prod1 = new Production { Predecessor = predessor, Successor = successor };
+            var prod2 = new Production { Predecessor = predessor, Successor = successor };
+
+            Assert.AreEqual(prod2, prod1);
+
+            Assert.AreEqual(prod1.GetHashCode(), prod2.GetHashCode());
+        }
+
+        /// <summary>
+        /// Verifies that the equality operator for <see cref="Production"/>, when both <see cref="Production.Predecessor"/> and <see cref="Production.Successor"/> values are equal, properly returns <c>true</c>
+        /// </summary>
+        [TestMethod]
+        public void Operator_Equality_Predecessor_and_Successor_are_equal_returns_true()
+        {
+            var predessor = "a";
+            var successor = "b";
+
+            var prod1 = new Production
+            {
+                Predecessor = predessor,
+                Successor = successor
+            };
+
+            var prod2 = new Production
+            {
+                Predecessor = predessor,
+                Successor = successor
+            };
+
+            Assert.AreEqual(prod1.Predecessor, prod2.Predecessor);
+            Assert.AreEqual(prod1.Successor, prod2.Successor);
+
+            var expected = true;
+            var actual = prod1 == prod2;
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        /// <summary>
+        /// Verifies the equality operator for <see cref="Production"/>, when <see cref="Production.Predecessor"/> values are not equal, properly returns <c>false</c>
+        /// </summary>
+        [TestMethod]
+        public void Operator_Equality_Predecessor_isNot_equal_returns_false()
+        {
+            var predessor1 = "a";
+            var predessor2 = "b";
+            var successor = "c";
+
+            var prod1 = new Production
+            {
+                Predecessor = predessor1,
+                Successor = successor
+            };
+
+            var prod2 = new Production
+            {
+                Predecessor = predessor2,
+                Successor = successor
+            };
+
+            Assert.AreNotEqual(prod1.Predecessor, prod2.Predecessor);
+
+            var expected = false;
+            var actual = prod1 == prod2;
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        /// <summary>
+        /// Verifies the equality operator for <see cref="Production"/>, when <see cref="Production.Successor"/> values are not equal, properly returns <c>false</c>
+        /// </summary>
+        [TestMethod]
+        public void Operator_Equality_Successor_isNot_equal_returns_false()
+        {
+            var predessor1 = "a";
+            var predessor2 = "b";
+            var successor = "c";
+
+            var prod1 = new Production
+            {
+                Predecessor = predessor1,
+                Successor = successor
+            };
+
+            var prod2 = new Production
+            {
+                Predecessor = predessor2,
+                Successor = successor
+            };
+
+            Assert.AreNotEqual(prod1.Predecessor, prod2.Predecessor);
+
+            var expected = false;
+            var actual = prod1 == prod2;
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        /// <summary>
+        /// Verifies that the inequality operator for <see cref="Production"/>, when both <see cref="Production.Predecessor"/> and <see cref="Production.Successor"/> values are equal, properly returns <c>false</c>
+        /// </summary>
+        [TestMethod]
+        public void Operator_Inequality_Predecessor_and_Successor_are_equal_returns_false()
+        {
+            var predessor = "a";
+            var successor = "b";
+
+            var prod1 = new Production
+            {
+                Predecessor = predessor,
+                Successor = successor
+            };
+
+            var prod2 = new Production
+            {
+                Predecessor = predessor,
+                Successor = successor
+            };
+
+            Assert.AreEqual(prod1.Predecessor, prod2.Predecessor);
+            Assert.AreEqual(prod1.Successor, prod2.Successor);
+
+            var expected = false;
+            var actual = prod1 != prod2;
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        /// <summary>
+        /// Verifies the inequality operator for <see cref="Production"/>, when <see cref="Production.Predecessor"/> values are not equal, properly returns <c>true</c>
+        /// </summary>
+        [TestMethod]
+        public void Operator_Inequality_Predecessor_isNot_equal_returns_true()
+        {
+            var predessor1 = "a";
+            var predessor2 = "b";
+            var successor = "c";
+
+            var prod1 = new Production
+            {
+                Predecessor = predessor1,
+                Successor = successor
+            };
+
+            var prod2 = new Production
+            {
+                Predecessor = predessor2,
+                Successor = successor
+            };
+
+            Assert.AreNotEqual(prod1.Predecessor, prod2.Predecessor);
+
+            var expected = true;
+            var actual = prod1 != prod2;
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        /// <summary>
+        /// Verifies the inequality operator for <see cref="Production"/>, when <see cref="Production.Successor"/> values are not equal, properly returns <c>true</c>
+        /// </summary>
+        [TestMethod]
+        public void Operator_Inequality_Successor_isNot_equal_returns_true()
+        {
+            var predessor1 = "a";
+            var predessor2 = "b";
+            var successor = "c";
+
+            var prod1 = new Production
+            {
+                Predecessor = predessor1,
+                Successor = successor
+            };
+
+            var prod2 = new Production
+            {
+                Predecessor = predessor2,
+                Successor = successor
+            };
+
+            Assert.AreNotEqual(prod1.Predecessor, prod2.Predecessor);
+
+            var expected = true;
+            var actual = prod1 != prod2;
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        /// <summary>
         /// Verifies the <see cref="Production.Predecessor"/> initializer, when the initial value is empty, properly throws an <see cref="ArgumentException"/>
         /// </summary>
         [TestMethod]
